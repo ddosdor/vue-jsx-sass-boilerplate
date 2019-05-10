76f413993c84018efd7fd116f2862d2ea55edd7f
Sebastian Drzewicki <ddosdor@gmail.com>
2019-05-10 22:04:10 +0200

    (fix): Fix some typo

--- src/components/HelloVue/index.jsx
+++ src/components/HelloVue/index.jsx
@@ -10,12 +10,12 @@ export default {
       <div class={ style['HelloVue'] }>
         <h1 class={ style['HelloVue__title'] }>Welcome to Vue + JSX</h1>
         <p>
-          Vue application bolierplate with JSX, Sass and CSS Modules
+          Vue application boilerplate with JSX, Sass and CSS Modules
         </p>
         <a class={ StringTypes(Button['button'], style['HelloVue__link']) }
           href="https://github.com/ddosdor/vue-jsx-sass-boilerplate" target="_blank"
           rel="noopener noreferrer"
-        >Se me on Github</a>
+        >See me on Github</a>
       </div>
     );
   },
